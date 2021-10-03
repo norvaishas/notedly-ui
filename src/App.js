6da30027b8f2c15916f74a6267fedd497ecb0e3f
Sergei Norvaishas <snorvaishas@yandex.ru>
2021-10-03 17:15:24 +0300

    feat: config apolo client and connect it to API (chapter 14)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import Pages from './pages';
 import GlobalStyle from './components/GlobalStyle';
 
+// Настраиваем API URI и кэш
+const uri = process.env.API_URI;
+const cache = new InMemoryCache();
+
+// Настраиваем Apollo Client
+const client = new ApolloClient({
+  uri,
+  cache,
+  connectToDevTools: true
+});
+
 const App = () => {
   return (
-    <>
+    <ApolloProvider client={client}>
       <GlobalStyle/>
       <Pages/>
-    </>
+    </ApolloProvider>
   );
 };
 
