141d3763353b9439268c2f5177767e84ca4e1ac4
Sergei Norvaishas <snorvaishas@yandex.ru>
2021-10-04 18:48:41 +0300

    feat: add styles for notefeed and note (chapter 14)

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,18 +1,53 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+// Импортируем утилиту форматирования из 'date-fns`
+import { format } from 'date-fns';
+import styled from 'styled-components';
+
+const StyledNote = styled.article`
+  max-width: 750px;
+  margin: 0 auto;
+`;
+
+// Стилизуем метаданные заметки
+const MetaData = styled.div`
+  @media (min-width: 500px) {
+    display: flex;
+    align-items: top;
+  }
+`;
+
+// Добавляем пространство между аватаром и метаданными
+const MetaInfo = styled.div`
+  padding-right: 1em;
+`;
+// Выравниваем 'UserActions' по правой стороне на больших экранах
+const UserActions = styled.div`
+  margin-left: auto;
+`;
 
 const Note = ({ note }) => {
   return (
-    <article>
-      <img
-        src={note.author.avatar}
-        alt={`${note.author.username} avatar`}
-        height="50px"
-      />{' '}
-      {note.author.username} {note.createdAt} {note.favoriteCount}{' '}
+    <StyledNote>
+      <MetaData>
+        <MetaInfo>
+          <img
+            src={note.author.avatar}
+            alt={`${note.author.username} avatar`}
+            height="50px"
+          />
+        </MetaInfo>
+        <MetaInfo>
+          <em>by</em> {note.author.username} <br />
+          {format(note.createdAt, 'Do MMM YYYY')}
+        </MetaInfo>
+        <UserActions>
+          <em>Favorites:</em> {note.favoriteCount}
+        </UserActions>
+      </MetaData>
       <ReactMarkdown source={note.content} />
-    </article>
-  )
-}
+    </StyledNote>
+  );
+};
 
 export default Note;
