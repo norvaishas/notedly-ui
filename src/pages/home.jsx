235a54d8a72f2e33791d3ca365c120ffc3f8e98d
Sergei Norvaishas <snorvaishas@yandex.ru>
2021-10-10 13:57:34 +0300

    feat: implement pagination with use fetchMore from useQuery hook (ch. 14)

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -37,9 +37,38 @@ const Home = () => {
 
   return (
     <div>
-      <p>The data loaded!</p>
       <NoteFeed notes={data.noteFeed.notes} />
-      <Button>Click Me!</Button>
+      {data.noteFeed.hasNextPage &&
+        <Button
+          onClick={() =>
+            // Выполняем новый запрос с помощью метода fetchMore
+            fetchMore({
+              // в который передаем значение cursor, полученное из предыдущего запроса
+              variables: {
+                cursor: data.noteFeed.cursor
+              },
+              // updateQuery обновляет значения cursor и hasNextPage, а также совмещает результаты в один массив
+              updateQuery: (previousResult, { fetchMoreResult }) => {
+                return {
+                  noteFeed: {
+                    cursor: fetchMoreResult.noteFeed.cursor,
+                    hasNextPage: fetchMoreResult.noteFeed.hasNextPage,
+                    // Совмещаем новые результаты со старыми
+                    notes: [
+                      ...previousResult.noteFeed.notes,
+                      ...fetchMoreResult.noteFeed.notes
+                    ],
+                    // имя запроса, которое включается в результаты Apollo
+                    __typename: 'noteFeed',
+                  },
+                };
+              }
+            })
+          }
+        >
+          Load more
+        </Button>
+      }
     </div>
   );
 };
