cbf19883704beb57303b18d93ec55cf69e033fac
Sergei Norvaishas <snorvaishas@yandex.ru>
2021-10-16 11:11:44 +0300

    feat: attach token to request header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from 'apollo-link-context';
+
 import Pages from './pages';
 import GlobalStyle from './components/GlobalStyle';
 
 // Настраиваем API URI и кэш
 const uri = process.env.API_URI;
 const cache = new InMemoryCache();
+const httpLink = createHttpLink({ uri });
+
+// Проверяем наличие токена и возвращаем заголовки в контекст
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: localStorage.getItem('token') || ''
+    }
+  };
+});
 
 // Настраиваем Apollo Client
 const client = new ApolloClient({
-  uri,
+  link: authLink.concat(httpLink),
   cache,
+  resolvers: {},
   connectToDevTools: true
 });
 
